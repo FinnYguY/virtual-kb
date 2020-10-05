613678aefe3541fc87fa81a643ca82f5da89aeeb
finnyguy <tema.menschikoff2013165@gmail.com>
2020-10-05 10:36:31 +0300

    feat: freezed keyboard constants

--- src/js/constants.js
+++ src/js/constants.js
@@ -1,16 +1,17 @@
-// export const kbEngLower = ['<sup>~</sup> <sub>\`</sub>', '1<sup>!</sup>', '2<sup>@</sup>', '3<sup>#</sup>', '4<sup>$</sup>', '5<sup>%</sup>', '6<sup>^</sup>', '7<sup>&</sup>', '8<sup>*</sup>', '9<sup>(</sup>', '0<sup>)</sup>', '_ -', '+=', 'Backspace', 'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[ {', '] }', '| \\', 'Del', 'Caps Lock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ': ;', '\" \'', 'Enter', 'Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', '< ,', '> .', '? /', '<i class="fas fa-angle-up"></i>', 'Shift', 'Ctrl', '<i class="fab fa-windows"></i>', 'Alt', '', 'Alt', 'Ctrl', '<i class="fas fa-angle-left"></i>', '<i class="fas fa-angle-down"></i>', '<i class="fas fa-angle-right"></i>', 'Info'];
-//
-// export const kbEngUpper = ['<sup>~</sup> <sub>\`</sub>', '1<sup>!</sup>', '2<sup>@</sup>', '3<sup>#</sup>', '4<sup>$</sup>', '5<sup>%</sup>', '6<sup>^</sup>', '7<sup>&</sup>', '8<sup>*</sup>', '9<sup>(</sup>', '0<sup>)</sup>', '_ -', '+=', 'Backspace', 'Tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '[ {', '] }', '| \\', 'Del', 'Caps Lock', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ': ;', '\" \'', 'Enter', 'Shift', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '< ,', '> .', '? /', '<i class="fas fa-angle-up"></i>', 'Shift', 'Ctrl', '<i class="fab fa-windows"></i>', 'Alt', '', 'Alt', 'Ctrl', '<i class="fas fa-angle-left"></i>', '<i class="fas fa-angle-down"></i>', '<i class="fas fa-angle-right"></i>', 'Info'];
-
 export const kbEngLower = ['\`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', 'Del', 'Caps Lock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', 'Enter', 'Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '↑', 'Shift', 'Ctrl', '❖', 'Alt', '', 'Alt', 'Ctrl', '←', '↓', '→', 'Info'];
+Object.freeze(kbEngLower);
 
 export const kbEngUpper = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', 'Backspace', 'Tab', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', '|', 'Del', 'Caps Lock', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':', '\"', 'Enter', 'Shift', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?', '↑', 'Shift', 'Ctrl', '❖', 'Alt', '', 'Alt', 'Ctrl', '←', '↓', '→', 'Info'];
+Object.freeze(kbEngUpper);
 
 export const kbRusLower = ['ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '\\', 'Del', 'Caps Lock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'Enter', 'Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', '↑', 'Shift', 'Ctrl', '❖', 'Alt', '', 'Alt', 'Ctrl', '←', '↓', '→', 'Info'];
+Object.freeze(kbRusLower);
 
 export const kbRusUpper = ['Ё', '!', '\"', '№', ';', '%', ':', '?', '*', '(', ')', '_', '+', 'Backspace', 'Tab', 'Й', 'Ц', 'У', 'К', 'Е', 'Н', 'Г', 'Ш', 'Щ', 'З', 'Х', 'Ъ', '/', 'Del', 'Caps Lock', 'Ф', 'Ы', 'В', 'А', 'П', 'Р', 'О', 'Л', 'Д', 'Ж', 'Э', 'Enter', 'Shift', 'Я', 'Ч', 'С', 'М', 'И', 'Т', 'Ь', 'Б', 'Ю', ',', '↑', 'Shift', 'Ctrl', '❖', 'Alt', '', 'Alt', 'Ctrl', '←', '↓', '→', 'Info'];
+Object.freeze(kbRusUpper);
 
 export const kbClass = [' Backquote', ' Digit1', ' Digit2', ' Digit3', ' Digit4', ' Digit5', ' Digit6', ' Digit7', ' Digit8', ' Digit9', ' Digit0', ' Minus', ' Equal', ' -functional -backspace Backspace', ' -functional -tab Tab', ' KeyQ', ' KeyW', ' KeyE', ' KeyR', ' KeyT', ' KeyY', ' KeyU', ' KeyI', ' KeyO', ' KeyP', ' BracketLeft', ' BracketRight', ' Backslash', ' -functional -del Delete', ' -functional -caps CapsLock', ' KeyA', ' KeyS', ' KeyD', ' KeyF', ' KeyG', ' KeyH', ' KeyJ', ' KeyK', ' KeyL', ' Semicolon', ' Quote', ' -functional -enter Enter', ' -functional -lshift ShiftLeft', ' KeyZ', ' KeyX', ' KeyC', ' KeyV', ' KeyB', ' KeyN', ' KeyM', ' Comma', ' Period', ' Slash', ' -arrow ArrowUp', ' -functional -rshift ShiftRight', ' -functional -lctrl ControlLeft', ' -functional -win MetaLeft', ' -functional AltLeft', ' -functional -space Space', ' -functional AltRight', ' -functional ControlRight', '  -arrow ArrowLeft', ' -arrow ArrowDown', ' -arrow ArrowRight', ' -functional -info'];
+Object.freeze(kbClass);
 
 export const arrowRegExp = /^Arrow.+$/;
 
